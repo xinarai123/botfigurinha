0bc80de820dcb8c6fc8c85c82444bfb2d2160570
xinarai123 <77704075+xinarai123@users.noreply.github.com>
2021-01-19 21:32:25 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,40 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do XINARAI:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  EDITADO POR: XINARAI
+  TRADUZIDO POR: DOKA
+╚════════════════════`
+}
+
+exports.dokamenu = dokamenu
+
+
+
+
+
+
+
+
+
